9960350a970bedc90662a1d118977d9d7194da6d
Fiskoff <fiskoff2015@yandex.ru>
2026-04-09 21:46:19 +0700

    Типы данных, Шаблонная строка, Конвертация типов

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-// let - ключевое слово, указывает на создание переменной
-let firstNumber = 5;
-firstNumber = 10;
+// Объекты(изменяемые) и примитивы(не изменяемые)
+// Примитивные типы: числа, строки, логический тип, null(намеренное отсутствие значения), undefined(значение ещё не присвоено), Symbol(уникальное неизменяемое значение)
 
-// const - ключевое слово, указывает на создание переменной
-// если переменная не будет изменяться, то используем const
-const secondNumber = 5;
+
+
+const projectName = 'Internet Store';
+const price = 2000;
+// Шаблонная строка
+const fullString = `Проект: ${projectName} - цена: ${price}`
+
+
+
+// Конвертация типов
+const age = '18'
+const ageNumber = Number(age)
+const ageString = String(ageNumber)
+
+// false
+const valFalse1 = Boolean(0)
+const valFalse2 = Boolean('')
+const valFalse3 = Boolean(null)
+const valFalse4 = Boolean(undefined)
+const valFalse5 = Boolean(NaN)  // или Boolean(Number("age"))
