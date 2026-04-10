4fc7fe821194ffd0ac1ba09f8e2f1c14407b6bb7
Fiskoff <fiskoff2015@yandex.ru>
2026-04-10 16:12:25 +0700

    Массивы и Деструктуризация

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-// Обычная функция
-function powerOfTwo(number) {
-  return number * number;
-}
+const roles = ["admin", "moderator", "user"]
+
+const lengthRoles = roles.length;  // длинна массива
+
+let firsValue = roles[0];  // получение
+roles[0] = "superAdmin";  // изменение
+
+roles.push("newRole");  // добавление
+roles.unshift();  // добавление в начало
+
+const deleteLastElement = roles.pop();  // удаление последнего элемента и возврат его
+const deleteFirstElement = roles.shift();  // удаление первого элемента и возврат его
+
+const index = roles.indexOf("admin");  // вернёт индекс элемента по значению (первое вхождение). Если элемента нет, то вернётся -1
+roles.includes("admin");  // вернёт true, если элемент есть в массиве, иначе false
+
+// slice(start, end) - возвращает новый массив, в котором будут элементы из диапазона start до end (не включая end)
+roles.slice(1, 3);  // вернёт массив с элементами с индексами 1 и 2
+roles.slice(1);  // вернёт массив с элементами с индекса 1 и дальше
+
+// splice(start, deleteCount) - возвращает изменённый массив
+roles.splice(1); //  вернёт массив с элементами с индекса 1
+roles.splice(1, 2); //  вернёт массив с элементами с индекса 1 и 2
+
+roles.reverse();  // перевернёт массив
+roles.sort();  //  сортирует массив
+
+roles.concat(['admin+', 'user+']);  //  объединяет массивы
 
 
-// Анонимная функция, используется там, где объявлена(в одной области видимости)
-const powerOfTwoNotName = function(number) {
-  return number * number;
-};
+
+const url = "auth/user/login"
+const urlInList = url.split('/')  // вернёт массив из элементов строки разделённой /
+const listInUrl = urlInList.join('/')  // строку из элементов массива разделённых /
 
 
-// стрелочные функции, аргументы  => результат
-const powerOfTwoArrow = number => number * number;
-// или
-const powerOfTwoArrow2 = (number, i) => {
-    console.log(i);
-    return number * number
-};
+// деструктуризация - распаковка значений массива иои объекта в переменные одной строкой
+const getRoles = () => ['admin', 'moderator', 'user', 'guest', 'visitor']
+
+// деструктуризация
+const [first, second, third] = getRoles()
+console.log(first)
+console.log(second)
+console.log(third)
+
+// выборочная деструктуризация
+const [first2, _, third2] = getRoles()  // Распечатается только первый и третий элементы
+
+// Rect(...) оператор
+const [first3, second3, ...others] = getRoles()  // first3, second3 - первые два элемента, others - массив из остальных элементов
