047b1091df0f486e869668df7bcddbe4d80e6b8b
Fiskoff <fiskoff2015@yandex.ru>
2026-04-10 16:49:35 +0700

    Циклы
    - for
    - while
    - for of

--- app.js
+++ app.js
@@ -1,50 +1,38 @@
-const roles = ["admin", "moderator", "user"]
+const tasks = [
+	'Задача 1',
+	'Задача 2',
+	'Задача 3',
+	'Задача 4',
+	'Задача 5',
+	'Задача 6',
+	'Задача 7',
+	'Задача 8',
+]
 
-const lengthRoles = roles.length;  // длинна массива
+for (let i = 0; i < tasks.length; i++) {
+	if (tasks[i] === 'Задача 3') {
+		continue
+	}
 
-let firsValue = roles[0];  // получение
-roles[0] = "superAdmin";  // изменение
+	if (tasks[i] === 'Задача 5') {
+		break
+	}
 
-roles.push("newRole");  // добавление
-roles.unshift();  // добавление в начало
+	console.log(tasks[i])
+}
 
-const deleteLastElement = roles.pop();  // удаление последнего элемента и возврат его
-const deleteFirstElement = roles.shift();  // удаление первого элемента и возврат его
+let i = 0
+while (tasks[i] !== 'Задача 5') {
+	console.log(tasks[i])
+	i++
+}
 
-const index = roles.indexOf("admin");  // вернёт индекс элемента по значению (первое вхождение). Если элемента нет, то вернётся -1
-roles.includes("admin");  // вернёт true, если элемент есть в массиве, иначе false
+// итерируется по значениям массива
+for (let element of tasks) {
+	console.log(element)
+}
 
-// slice(start, end) - возвращает новый массив, в котором будут элементы из диапазона start до end (не включая end)
-roles.slice(1, 3);  // вернёт массив с элементами с индексами 1 и 2
-roles.slice(1);  // вернёт массив с элементами с индекса 1 и дальше
-
-// splice(start, deleteCount) - возвращает изменённый массив
-roles.splice(1); //  вернёт массив с элементами с индекса 1
-roles.splice(1, 2); //  вернёт массив с элементами с индекса 1 и 2
-
-roles.reverse();  // перевернёт массив
-roles.sort();  //  сортирует массив
-
-roles.concat(['admin+', 'user+']);  //  объединяет массивы
-
-
-
-const url = "auth/user/login"
-const urlInList = url.split('/')  // вернёт массив из элементов строки разделённой /
-const listInUrl = urlInList.join('/')  // строку из элементов массива разделённых /
-
-
-// деструктуризация - распаковка значений массива иои объекта в переменные одной строкой
-const getRoles = () => ['admin', 'moderator', 'user', 'guest', 'visitor']
-
-// деструктуризация
-const [first, second, third] = getRoles()
-console.log(first)
-console.log(second)
-console.log(third)
-
-// выборочная деструктуризация
-const [first2, _, third2] = getRoles()  // Распечатается только первый и третий элементы
-
-// Rect(...) оператор
-const [first3, second3, ...others] = getRoles()  // first3, second3 - первые два элемента, others - массив из остальных элементов
+// итерируется по индексам массива
+for (let index in tasks) {
+	console.log(index)
+}
