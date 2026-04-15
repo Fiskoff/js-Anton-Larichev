d4829f63dea70cd3c9ed539c9d22038920313d2f
Fiskoff <fiskoff2015@yandex.ru>
2026-04-15 11:09:32 +0700

    DOM Обработка нажатия

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// DOM (Document Object Model) - это объект представление исходного HTML-кода документа
-// DOM  позволяет программно, при помощи js, манипулировать HTML-кодом
+'use strict'
 
-'use strict' // это режим, в котором JavaScript не пропускает ошибки, которые раньше игнорировал, и помогает избежать неожиданного поведения кода. 
+function changeClick() {
+    const input = document.querySelector('.input-text').value;
+    document.querySelector('.panel').innerText = input;
+    document.querySelector(".input-text").value = "";
+};
 
-// document - корневой элемент документа, даёт доступ к элементам страницы
-
-const panelText = document.querySelector('.panel').innerText;  // получаем текст
-console.log(panelText);
-document.querySelector('.panel').innerText = 'Привет, мир!';  // изменяем текст
-document.querySelector('.panel').style.backgroundColor = 'red';  // устанавливает стиль
+// через querySelector обращаемся к элементу, addEventListener обработчик событий
+document.querySelector('.input-btn').addEventListener('click', changeClick);
