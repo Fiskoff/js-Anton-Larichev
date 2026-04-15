afcaa765b5511510b7e5ef41414ad1d4afc1cc7e
Fiskoff <fiskoff2015@yandex.ru>
2026-04-15 11:54:37 +0700

    DOM Работа со стилями и классами

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ function changeClick() {
     const input = document.querySelector('.input-text').value;
     document.querySelector('.panel').innerText = input;
     document.querySelector(".input-text").value = "";
-};
 
+    // 1 вариант изменения стиля - не самый удобный, так как нужно прописывать свойства в js файле
+    document.querySelector('.notification').style.display = "block";
+    // 2 вариант изменения стиля - выбранному объекту подключаю ещё один класс, который переопределит нужные свойства
+    document.querySelector('.notification').classList.add('notification_active')
+};
 
 document.querySelector('.input-btn').addEventListener('click', changeClick);
 document.querySelector('.input-text').addEventListener('keydown', e => {  // e - event хранит всю информацию о событии
